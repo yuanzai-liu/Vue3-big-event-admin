19fd26fd70812de9624552b28c14597c887ab449
yuanzia-liu <2523632216@qq.com>
2024-10-02 01:16:15 +0800

    LoginPage页面的调整

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,8 @@
-<script setup>
-// import { useRoute, useRouter } from 'vue-router'
-import { useUserStore } from '@/stores'
-// const router = useRouter()
-// const route = useRoute()
-const userStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
     <router-view></router-view>
-
-    <hr />
-    <hr />
-    <el-button type="success">Success</el-button>
-
-    {{ userStore.token }}
-    <el-button @click="userStore.setToken('login')">登录</el-button>
-    <el-button @click="userStore.removeToken()">退出</el-button>
   </div>
 </template>
 
